d848895eba5ab3dbe4a580fcda155a0f2fb90cde
kitUIN <8119808+kituin@user.noreply.gitee.com>
2020-12-31 23:40:57 +0800

    增加Iqdb支持

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import setuptools
+
 requirements = [
-    requirement.strip() for requirement in open('requirements.txt','r',encoding='utf-8').readlines()
+    requirement.strip() for requirement in open('requirements.txt', 'r', encoding='utf-8').readlines()
 ]
 
-with open("README.md", "r",encoding='utf-8') as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="PicImageSearch",
-    version="0.6.7",
+    version="0.7.0",
     author="kitUIN",
     author_email="kulujun@gmail.com",
     description="PicImageSearch APIs for Python 3.x 适用于 Python 3 以图搜源整合API",
@@ -20,6 +21,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-    ],install_requires=requirements,
+    ], install_requires=requirements,
     python_requires='>=3.6',
 )
