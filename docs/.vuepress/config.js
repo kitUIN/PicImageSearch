571a60758776ed64636627eb3dbae8887299ae73
kitUIN <kulujun@gmail.com>
2022-07-31 23:18:15 +0800

    docs init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,192 @@
+const baiduCode = require('./config/baiduCode.js'); // 百度统计hm码
+const htmlModules = require('./config/htmlModules.js');
+
+
+module.exports = {
+
+    theme: 'vdoing', // 使用依赖包主题
+    // theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
+    title: "PicImageSearch",
+    description: '✨ 聚合识图引擎 用于以图搜源✨',
+    // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+    head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+        ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+        ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ],
+
+    // 主题配置
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '指南',
+                link: '/wiki/picimagesearch/',
+                items: [
+                    { text: 'SauceNAO', link: '/wiki/picimagesearch/SauceNAO/DataStructure/' },
+                    { text: 'TraceMoe', link: '/wiki/picimagesearch/TraceMoe/DataStructure/' },
+                    { text: 'Ascii2D', link: '/wiki/picimagesearch/Ascii2D/DataStructure/' },
+                    { text: 'Iqdb', link: '/wiki/picimagesearch/Iqdb/DataStructure/' },
+                    { text: 'E-hentai', link: '/wiki/picimagesearch/E-hentai/DataStructure/' },
+                    { text: 'Google识图', link: '/wiki/picimagesearch/Google/DataStructure/' },
+                    { text: 'BaiDu识图', link: '/wiki/picimagesearch/BaiDu/DataStructure/' },
+                    { text: '更新日志', link: '/wiki/picimagesearch/releases' },
+
+                ]
+            }
+
+        ],
+        sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+        logo: '/img/logo.png', // 导航栏logo
+        repo: 'kitUIN/PicImageSearch', // 导航栏右侧生成Github链接
+        searchMaxSuggestions: 10, // 搜索结果显示最大数
+        lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+
+        // docsDir: 'docs', // 编辑的文件夹
+        // editLinks: true, // 编辑链接
+        // editLinkText: '编辑',
+
+        // 以下配置是Vdoing主题改动的和新增的配置
+        sidebar: { mode: 'structuring', collapsable: true }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+
+        // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
+        updateBar: { // 最近更新栏
+            showToArticle: false, // 显示到文章页底部，默认true
+            // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+        },
+        // titleBadge: false, // 文章标题前的图标是否显示，默认true
+        // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
+        //   '图标地址1',
+        //   '图标地址2'
+        // ],
+
+        pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
+
+        // contentBgStyle: 1,
+
+        category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
+        tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
+        // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
+
+        author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
+            name: 'kitUIN', // 必需
+            href: 'https://github.com/kitUIN' // 可选的
+        },
+        social: { // 社交图标，显示于博主信息栏和页脚栏
+            // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
+            icons: [{
+                    iconClass: 'icon-youjian',
+                    title: '发邮件',
+                    link: 'mailto:kulujun@gmail.com',
+                },
+                {
+                    iconClass: 'icon-bilibili',
+                    title: 'BILIBILI',
+                    link: 'https://space.bilibili.com/61924180',
+                },
+                {
+                    iconClass: 'icon-github',
+                    title: 'GitHub',
+                    link: 'https://github.com/kitUIN',
+                },
+            ],
+        },
+        footer: {
+            // 页脚信息
+            createYear: 2021, // 博客创建年份
+            copyrightInfo: 'kitUIN | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+        },
+        htmlModules,
+    },
+
+    // 插件
+    plugins: [
+        // [require('./plugins/love-me'), { // 鼠标点击爱心特效
+        //   color: '#11a8cd', // 爱心颜色，默认随机色
+        //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
+        // }],
+
+        ['fulltext-search'], // 全文搜索
+        [
+            'vuepress-plugin-comment-plus',
+            {
+                choosen: 'waline',
+                // options选项中的所有参数，会传给Waline的配置
+                options: {
+                    el: '#valine-vuepress-comment',
+                    serverURL: 'https://kituin-comments.vercel.app/', //  例如 "https://***.vercel.app/"
+                    path: '<%- frontmatter.commentid || frontmatter.permalink %>',
+                    emoji: [
+                        'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/weibo',
+                        'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili',
+                    ],
+                    placeholder: "请留言"
+                }
+            }
+        ],
+        // ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+        //   thirdparty: [ // 可选，默认 []
+        //     {
+        //       title: '在GitHub中搜索',
+        //       frontUrl: 'https://github.com/search?q=', // 搜索链接的前面部分
+        //       behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
+        //     },
+        //     {
+        //       title: '在npm中搜索',
+        //       frontUrl: 'https://www.npmjs.com/search?q=',
+        //     },
+        //     {
+        //       title: '在Bing中搜索',
+        //       frontUrl: 'https://cn.bing.com/search?q='
+        //     }
+        //   ]
+        // }],
+
+
+
+        ['one-click-copy', { // 代码块复制按钮
+            copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+            copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+            duration: 1000, // prompt message display time.
+            showInMobile: false // whether to display on the mobile side, default: false.
+        }],
+        ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+            settings: {
+                // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+                // cssLib: ['http://xxx'], // 在线示例中的css依赖
+                // vue: 'https://fastly.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+                jsfiddle: false, // 是否显示 jsfiddle 链接
+                codepen: true, // 是否显示 codepen 链接
+                horizontal: false // 是否展示为横向样式
+            }
+        }],
+        [
+            'vuepress-plugin-zooming', // 放大图片
+            {
+                selector: '.theme-vdoing-content img:not(.no-zoom)',
+                options: {
+                    bgColor: 'rgba(0,0,0,0.6)'
+                },
+            },
+        ],
+        [
+            '@vuepress/last-updated', // "上次更新"时间格式
+            {
+                transformer: (timestamp, lang) => {
+                    const dayjs = require('dayjs') // https://day.js.org/
+                    return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+                },
+            }
+        ]
+    ],
+
+    markdown: {
+        // lineNumbers: true,
+        extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+    },
+
+    // 监听文件变化并重新构建
+    extraWatchFiles: [
+        '.vuepress/config.js',
+        '.vuepress/config/htmlModules.js',
+    ]
+}
---
+++ docs/index.md
@@ -0,0 +1,110 @@
+---
+home: true
+
+heroText: PicImageSearch
+tagline: ✨ 聚合识图引擎 用于以图搜源✨
+actionText: 开始使用 →
+actionLink: /wiki/picimagesearch/
+bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+
+# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+postList: none
+---
+<p align="center">
+  <a href="https://raw.githubusercontent.com/kitUIN/PicImageSearch/master/LICENSE">
+    <img src="https://img.shields.io/github/license/kitUIN/PicImageSearch" alt="license">
+  </a>
+  <a href="https://pypi.python.org/pypi/PicImageSearch">
+    <img src="https://img.shields.io/pypi/v/PicImageSearch" alt="pypi">
+  </a>
+  <img src="https://img.shields.io/badge/python-3.7+-blue" alt="python">
+  <a href="https://github.com/kitUIN/PicImageSearch/releases">
+    <img src="https://img.shields.io/github/v/release/kitUIN/PicImageSearch" alt="release">
+  </a>
+  <a href="https://github.com/kitUIN/PicImageSearch/issues">
+    <img src="https://img.shields.io/github/issues/kitUIN/PicImageSearch" alt="release">
+  </a>
+ </p>
+<p align="center">
+  <a href="https://github.com/kitUIN/PicImageSearch/compare/">✅提交PR</a>
+  ·
+  <a href="https://github.com/kitUIN/PicImageSearch/issues/new">🐛提交Bug</a>
+</p>
+
+## 🎉支持的搜图网站
+::: cardList 3
+```yaml
+- name: SauceNAO
+  desc: 'SauceNAO Reverse Image Search'
+  avatar: https://z3.ax1x.com/2021/03/21/64BclV.png
+  link: https://saucenao.com # 可选
+  bgColor: '#FFFFFF' # 可选，默认var(--bodyBg)。颜色值有#号时请添加单引号
+  textColor: '#6854A1' # 可选，默认var(--textColor)
+- name: TraceMoe
+  desc: 'Anime Scene Search by Image'
+  avatar: https://z3.ax1x.com/2021/03/21/64B6S0.png
+  link: https://trace.moe
+  bgColor: '#718971'
+  textColor: '#fff'
+- name: Ascii2D
+  desc: '二次元画像詳細検索'
+  avatar: https://z3.ax1x.com/2021/03/21/64BgyT.png
+  link: https://ascii2d.net
+  bgColor: '#40394a'
+  textColor: '#7eca9c'
+- name: Iqdb
+  desc: 'Multi-service image search'
+  avatar: https://z3.ax1x.com/2021/03/21/64BWmF.png
+  link: http://www.iqdb.org
+  bgColor: '#f39189'
+  textColor: '#046582'
+- name: Google谷歌识图
+  desc: '国外神器'
+  avatar: https://z3.ax1x.com/2021/03/21/64Bfw4.jpg
+  link: https://www.google.com/imghp
+  bgColor: '#caf7e3'
+  textColor: '#7868e6'
+- name: BaiDu百度识图
+  desc: '百度 已死'
+  avatar: https://z3.ax1x.com/2021/03/21/64B2OU.jpg
+  link: https://graph.baidu.com
+  bgColor: '#edeef7'
+  textColor: '#7868e6'
+- name: E-hentai/Exhentai
+  desc: 'E绅士'
+  avatar: https://pic2.zhimg.com/v2-17efcdb7f1eb08b53bba4cddf93a043a_1440w.jpg
+  link: https://e-hentai.org
+  bgColor: '#5b383f'
+  textColor: '#fff'
+```
+:::
+
+## 🎖贡献者
+::: cardList 3
+```yaml
+- name: NekoAria(Neko Aria) 
+  desc: 主要维护者
+  avatar: http://q1.qlogo.cn/g?b=qq&nk=990879119&s=640
+  link: https://github.com/NekoAria
+  bgColor: '#CBEAFA' 
+  textColor: '#6854A1'
+- name: kitUIN(Nacho)
+  desc: 项目拥有者
+  avatar: /img/logo.png
+  link: https://github.com/kitUIN
+  bgColor: '#40394a'
+  textColor: '#7eca9c'
+- name: lleans
+  desc: 贡献者
+  avatar: /img/2.jfif
+  link: https://github.com/lleans
+  bgColor: '#f39189'
+  textColor: '#046582'
+- name: chinoll
+  desc: 贡献者
+  avatar: /img/1.jfif
+  link: https://github.com/chinoll
+  bgColor: '#caf7e3'
+  textColor: '#7868e6'
+```
+:::
