fc0bd942b80ffdcb0891922ece1b9f4fbb644b56
Neko Aria <990879119@qq.com>
2023-01-04 21:56:09 +0800

    fix(ehentai): 修正搜索结果的解析逻辑

--- PicImageSearch/model/ehentai.py
+++ PicImageSearch/model/ehentai.py
@@ -22,9 +22,13 @@ class EHentaiItem:
             self.url = glink.parent("div").parent("a").attr("href")
         else:
             self.url = glink.parent("a").attr("href")
-        thumbnail = data.find(".glthumb img")
+        thumbnail = (
+            data.find(".glthumb img")
+            or data.find(".gl1e img")
+            or data.find(".gl3t img")
+        )
         self.thumbnail = thumbnail.attr("data-src") or thumbnail.attr("src")
-        self.type = data.find(".cn").eq(0).text()
+        self.type = data.find(".ct3").eq(0).text()
         self.date = data.find("[id^='posted']").eq(0).text()
         self.tags = [
             i.text() for i in data.find("div[class=gt],div[class=gtl]").items()
@@ -39,9 +43,10 @@ class EHentaiResponse:
         if "No unfiltered results found." in resp_text:
             self.raw = []
         else:
-            self.raw = [
-                EHentaiItem(i)
-                for i in data.find(".itg").children("tr").items()
-                if i.children("td")
-            ]
+            tr_items = data.find(".itg").children("tr").items()
+            if tr_items:
+                self.raw = [EHentaiItem(i) for i in tr_items if i.children("td")]
+            else:
+                gl1t_items = data.find(".itg").children(".gl1t").items()
+                self.raw = [EHentaiItem(i) for i in gl1t_items]
         self.url: str = resp_url
