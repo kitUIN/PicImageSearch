4cf59272e669982b87185cef069ae129645e7625
NekoAria <NekoAria@users.noreply.github.com>
2025-01-18 05:10:49 +0800

    refactor(copyseeker): optimize code structure and error handling

--- PicImageSearch/engines/copyseeker.py
+++ PicImageSearch/engines/copyseeker.py
@@ -1,6 +1,6 @@
-from json import loads as json_loads
-from json import dumps as json_dumps
 import os
+from json import dumps as json_dumps
+from json import loads as json_loads
 from pathlib import Path
 from typing import Any, Optional, Union
 
@@ -18,9 +18,7 @@ class Copyseeker(BaseSearchEngine[CopyseekerResponse]):
         base_url (str): The base URL for Copyseeker searches.
     """
 
-    def __init__(
-        self, base_url: str = "https://copyseeker.net", **request_kwargs: Any
-    ):
+    def __init__(self, base_url: str = "https://copyseeker.net", **request_kwargs: Any):
         """Initializes a Copyseeker API client.
 
         Args:
@@ -49,47 +47,46 @@ class Copyseeker(BaseSearchEngine[CopyseekerResponse]):
             - The discovery ID is required for retrieving search results.
         """
 
-        data = {"discoveryType": "ReverseImageSearch"}
+        resp = None
+        discovery_id = None
 
         if url:
-            data["imageUrl"] = url
+            data = {"discoveryType": "ReverseImageSearch", "imageUrl": url}
             headers = {
-                    "next-action": "408936c3bdf458fbec6cf3c1253f56aefbcb4cf509",
-                    "content-type": "text/plain;charset=UTF-8"
+                "next-action": "408936c3bdf458fbec6cf3c1253f56aefbcb4cf509",
+                "content-type": "text/plain;charset=UTF-8",
             }
 
             resp = await self._make_request(
                 method="post",
-                endpoint="",
-                data=json_dumps([data]),
                 headers=headers,
+                data=json_dumps([data]),
             )
         elif file:
-            headers = {
-                "next-action": "40dc303bfd320afd703a1a0a159464be4d64117f96",
-                "content-type": "multipart/form-data; boundary=-"
-            }
             files = {
                 "1_file": (os.path.basename(file), read_file(file), "image/jpeg"),
                 "1_discoveryType": (None, "ReverseImageSearch"),
-                "0": (None, '["$K1"]')
+                "0": (None, '["$K1"]'),
             }
-            files["1_discoveryType"] = (None, "ReverseImageSearch")
-            files["0"] = (None, '["$K1"]')
+            headers = {
+                "next-action": "40dc303bfd320afd703a1a0a159464be4d64117f96",
+                "content-type": "multipart/form-data; boundary=-",
+            }
 
             resp = await self._make_request(
                 method="post",
-                endpoint="",
+                headers=headers,
                 files=files,
-                headers=headers
             )
 
+        if resp:
             for line in resp.text.splitlines():
                 line = line.strip()
                 if line.startswith("1:{"):
-                    return json_loads(line[2:]).get("discoveryId")
-            return None
+                    discovery_id = json_loads(line[2:]).get("discoveryId")
+                    break
 
+        return discovery_id
 
     async def search(
         self,
@@ -131,16 +128,20 @@ class Copyseeker(BaseSearchEngine[CopyseekerResponse]):
 
         data = [{"discoveryId": discovery_id, "hasBlocker": "False"}]
         headers = {
-             "next-action": "4084b9ef4e0e6922ef12b23a4b8517be790fa67b88",
-             "content-type": "text/plain;charset=UTF-8"
+            "next-action": "4084b9ef4e0e6922ef12b23a4b8517be790fa67b88",
+            "content-type": "text/plain;charset=UTF-8",
         }
 
         resp = await self._make_request(
-            method="post", endpoint="discovery",data=json_dumps(data), headers=headers
+            method="post", endpoint="discovery", data=json_dumps(data), headers=headers
         )
-        
+
+        resp_json = {}
+
         for line in resp.text.splitlines():
             line = line.strip()
             if line.startswith("1:{"):
                 resp_json = json_loads(line[2:])
+                break
+
         return CopyseekerResponse(resp_json, resp.url)
